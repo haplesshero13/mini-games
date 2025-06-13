205175a1a683b9ca0f32eaf082d81c3d8ba0ff5d
Avery Yen <haplesshero13@users.noreply.github.com>
2025-06-13 15:58:56 -0400

    WIP router

---
+++ src/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const HomePage: React.FC = () => {
+  return (
+    <>
+      <h1>Welcome to Nofair Games!</h1>
+      <p>
+        Our goal is to entertain, and do no evil. This is a personal side
+        project, so any help is appreciated.
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
